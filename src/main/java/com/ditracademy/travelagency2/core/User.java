f6f2d640e47e814c006c98a9b192060f93f67333
wiem <wiemsghaierr@gmail.com>
2019-11-10 18:42:17 +0100

    lanbok

--- src/main/java/com/ditracademy/travelagency2/core/User.java
+++ src/main/java/com/ditracademy/travelagency2/core/User.java
@@ -1,11 +1,18 @@
 package com.ditracademy.travelagency2.core;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -14,36 +21,5 @@ public class User {
     private String name;
     private int age;
 
-    public User(){
-    }
 
-    public User(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
